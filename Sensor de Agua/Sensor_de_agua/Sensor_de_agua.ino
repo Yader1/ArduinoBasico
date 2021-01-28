e98ce2de880da7a1be8aaf96a0f696194e44961d
Yader_Campbell <yadermejiac123@gmail.com>
2021-01-27 22:40:01 -0600

    El sensor de agua detecta y enciente una led al ser positvo

--- Sensor de Agua/Sensor_de_agua/Sensor_de_agua.ino
+++ Sensor de Agua/Sensor_de_agua/Sensor_de_agua.ino
@@ -1,9 +1,22 @@
+#define LED 2
+int sensor;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sensor = analogRead(A0);
+  Serial.println(sensor);
+  delay(1000);
 
+  if(sensor > 0){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
+  delay(1000);
 }
